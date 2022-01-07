c7b6486f72e9e98e9b4d732d2a94ca9a9f1e3160
rupaak <rupakt525@gmail.com>
2022-01-07 23:39:30 +0545

    basic testing of react app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { fireEvent, render, waitFor } from "@testing-library/react";
+import App from "./App";
+import Counter from "./Counter";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("should take a snapshot", () => {
+  const { asFragment } = render(<App />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("should equal to 0", () => {
+  const { getByTestId } = render(<Counter />);
+  expect(getByTestId("counter")).toHaveTextContent("0");
+});
+
+it("should be enabled", () => {
+  const { getByTestId } = render(<Counter />);
+  expect(getByTestId("button-up")).not.toHaveAttribute("disabled");
+});
+
+it("should be disabled", () => {
+  const { getByTestId } = render(<Counter />);
+  expect(getByTestId("button-down")).toBeDisabled();
+});
+
+//event testing
+// it("increments counter", () => {
+//   const { getByTestId } = render(<Counter />);
+
+//   fireEvent.click(getByTestId("button-up"));
+
+//   expect(getByTestId("counter")).toHaveTextContent("1");
+// });
+
+// it("decrements counter", () => {
+//   const { getByTestId } = render(<Counter />);
+
+//   fireEvent.click(getByTestId("button-down"));
+
+//   expect(getByTestId("counter")).toHaveTextContent("-1");
+// });
+
+//asynchronous
+it("increments counter after 0.5s", async () => {
+  const { getByTestId, getByText } = render(<Counter />);
+
+  fireEvent.click(getByTestId("button-up"));
+
+  const counter = await waitFor(() => getByText("1"));
+
+  expect(counter).toHaveTextContent("1");
 });
